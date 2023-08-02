ac2259ac9634cc65a7724772652d82fbb51ec655
catzilla <kary.ishwaran@gmail.com>
2023-08-02 15:38:34 -0400

    working on terminal

--- js/console.js
+++ js/console.js
@@ -19,10 +19,21 @@ let windows = 1;
 let currentDirectory = '/';
 let directories = {
     '/': ['documents', 'pictures', 'videos'],
+    '/documents': [],
+    '/pictures': [],
+    '/videos': []
+};
+let files = {
     '/documents': ['file1.txt', 'file2.txt'],
     '/pictures': ['pic1.jpg', 'pic2.jpg'],
     '/videos': ['video1.mp4', 'video2.mp4']
 };
+let fileContents = {
+    '/documents/file1.txt': 'This is the content of file1.txt',
+    '/documents/file2.txt': 'This is the content of file2.txt',
+    // add other file contents here...
+};
+
 
 
 var term = $('.console').terminal({
@@ -49,17 +60,53 @@ var term = $('.console').terminal({
     },
 
     ls: function() {
-        this.echo(directories[currentDirectory].join('\n'));
+        let dirContents = directories[currentDirectory].concat(files[currentDirectory] || []);
+        this.echo(dirContents.join('\n'));
     },
     
     cd: function(dir) {
-        if (directories[currentDirectory].includes(dir)) {
-            currentDirectory += dir;
-            this.echo("Changed directory to " + currentDirectory);
+        if (dir === '..') {
+            currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf('/'));
+            if (!currentDirectory) currentDirectory = '/';
         } else {
-            this.echo("Invalid directory");
+            let newDirectory = currentDirectory === '/' ? currentDirectory + dir : currentDirectory + '/' + dir;
+            if (directories[newDirectory]) {
+                currentDirectory = newDirectory;
+            } else {
+                this.echo("Invalid directory");
+            }
+        }
+        this.echo("Current directory: " + currentDirectory);
+    },
+
+    open: function(file) {
+        let filePath = currentDirectory === '/' ? currentDirectory + file : currentDirectory + '/' + file;
+        if (fileContents[filePath]) {
+            let extension = file.split('.').pop();
+            switch (extension) {
+                case 'txt':
+                    this.echo(fileContents[filePath]);
+                    break;
+                case 'jpeg':
+                case 'jpg':
+                case 'gif':
+                case 'png':
+                    this.openImage(fileContents[filePath]);
+                    break;
+                case 'mp4':
+                case 'gif':
+                case 'apng':
+                    this.openMovingContent(fileContents[filePath]);
+                    break;
+                default:
+                    this.echo('Cannot open this file type');
+                    break;
+            }
+        } else {
+            this.echo("File not found");
         }
     },
+    
 
     clear: function() {
         this.clear();
@@ -75,7 +122,7 @@ var term = $('.console').terminal({
 
     openOtter: function() {
         this.echo("Opening otter window...");
-        openWindow(false, chooseOtterPic());
+        openWindow(false, chooseOtterPic(), 'image');
         this.echo("Opened otter window :)");
     },
 
@@ -83,7 +130,7 @@ var term = $('.console').terminal({
         // check if input is a valid image url
         if (image.match(/\.(jpeg|jpg|gif|png)$/) != null) {
             this.echo("Opening image window...");
-            openWindow(false, image);
+            openWindow(false, image, 'image');
             this.echo("Opened image window :)");
         }
         else {
@@ -91,9 +138,20 @@ var term = $('.console').terminal({
         }
     },
 
+    openMovingContent: function(content) {
+        if (content.match(/\.(mp4|gif|apng)$/) != null) {
+            this.echo("Opening content window...");
+            openWindow(false, content, 'video');
+            this.echo("Opened content window :)");
+        }
+        else {
+            this.echo("Invalid content url. The URL must end with .mp4, .gif or .apng");
+        }
+    },
+
     eval: function(expression) {
         try {
-            this.echo("Result: " + eval(expression));
+            this.echo("Result: " + math.evaluate(expression));
         } catch (e) {
             this.echo("Invalid mathematical expression");
         }
@@ -154,7 +212,7 @@ class BarAnimation extends $.terminal.Animation {
 term.echo(new BarAnimation(15)); // 50 frames per second
 
 // open window function
-function openWindow(text, image) {
+function openWindow(text, content, contentType) {
     // new div
     let newDiv = document.createElement("div");
     newDiv.classList.add("miniContentBox");
@@ -168,12 +226,23 @@ function openWindow(text, image) {
     newSpanTitle.innerHTML = "Window " + newDiv.id + `<button class="closeBtn" onclick="this.parentElement.parentElement.style.display='none'">x</button>`;
     newDiv.appendChild(newSpanTitle);
 
-    // if image:
-    if (image) {
-        let newDivImage = document.createElement("img");
-        newDivImage.src = image;
-        newDivImage.classList.add("contentImage");
-        newDiv.appendChild(newDivImage);
+    if (content) {
+        let newContentElement;
+        switch (contentType) {
+            case 'image':
+                newContentElement = document.createElement("img");
+                break;
+            case 'video':
+                newContentElement = document.createElement("video");
+                newContentElement.setAttribute('controls', '');
+                break;
+            default:
+                console.log('Unknown content type');
+                return;
+        }
+        newContentElement.src = content;
+        newContentElement.classList.add("contentImage");
+        newDiv.appendChild(newContentElement);
     }
     else {
         let newDivText = document.createElement("p");
@@ -181,11 +250,9 @@ function openWindow(text, image) {
         newDiv.appendChild(newDivText);
     }
 
-
     document.body.appendChild(newDiv);
-    
-
 }
+
 
 function chooseOtterPic() {
     var randomNum = Math.floor((Math.random() * 200) + 1);
